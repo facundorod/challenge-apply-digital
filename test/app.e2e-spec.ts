f1a5685794bf801582689284aa43138c87eec3ac
Facundo Rodriguez <rodriguezfacundohernan@gmail.com>
2024-09-13 11:17:25 -0300

    [e2e tests]: Use mock module instead of Infra module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import supertest from 'supertest';
 import { AppModule } from './../src/app.module';
+import { InfrastructureModule } from '@/infrastructure/infrastructure.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -9,7 +10,10 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideModule(InfrastructureModule)
+      .useModule(jest.fn())
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
